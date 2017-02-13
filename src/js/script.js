3d0d8765dfd9682501f85388108299290f8a631a
merlimar <merli.marvin@gmail.com>
2017-02-13 16:24:49 +0100

    ex 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,6 @@
-let iCurrentFigure = 0,
-	iFigureAmount,
-	aFigures;
-const fChangeFigure = function(){
-	aFigures[ iCurrentFigure ].classList.add("hide");
-	if ( ++iCurrentFigure === iFigureAmount){
-		iCurrentFigure = 0;
-	}
-	aFigures[ iCurrentFigure ].classList.remove("hide");
-}
+$( function(){
 
-const fHandleTab = function(oEvent){
-	oEvent.preventDefault();
-	let $elt= oEvent.currentTarget;
+	//1. a with rel external
+	$('a[rel*="external"]').attr("target", "_new");
 
-	if ($elt.parentNode.classList.contains("active")){
-		return;
-	}
-	document.querySelector("ul.nav.nav-tabs .active").classList.remove("active");
-	$elt.parentNode.classList.add("active");
-	document.querySelector(".tab-pane.active").classList.remove("active");
-	document.getElementById($elt.getAttribute("data-tab-target")).classList.add("active");
-};
-
-window.addEventListener( "load", function(){
-	//1. a with rel=external opens in new window
-	Array.from(document.querySelectorAll('a[rel*="external"]')).forEach(function($elt){
-		$elt.setAttribute("target","_new");
-	});
-	//2. handle tab
-	Array.from(document.querySelectorAll( "ul.nav.nav-tabs a")).forEach(function($elt){
-		$elt.addEventListener("click",fHandleTab);
-	});
-	//3. trombinoscope
-	aFigures = Array.from(document.querySelectorAll("#trombino figure"));
-	aFigures.forEach(function($elt, iIndex){
-		if (iIndex>0){
-			$elt.classList.add("hide")
-		}
-	});
-	iFigureAmount=aFigures.length;
-	setInterval( fChangeFigure, 1000);
-});
+})
